d56828980fa39824e8bd59750f388c69e8c83fc9
ytakano <ytakanoster@gmail.com>
2021-08-27 11:17:24 +0900

    Update hello.py

--- chap5/5.2/hello.py
+++ chap5/5.2/hello.py
@@ -2,8 +2,8 @@ def hello():
     print('Hello,', end='')
     yield  # ここで中断、再開 <1>
     print('World!')
-    yield  # ここで中断、再開 <2>
+    yield  # ここまで実行 (2)
 
-h = hello()  # 1まで実行
+h = hello()  # イテレータを生成
+h.__next__() # 1まで実行し中断
 h.__next__() # 1から再開し2まで実行
-h.__next__() # 2から再開

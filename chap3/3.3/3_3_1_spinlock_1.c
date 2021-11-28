6335ca5672b7d200b28afc006725447cafb82d25
ytakano <ytakanoster@gmail.com>
2021-11-28 23:56:46 +0900

    Update 3_3_1_spinlock_1.c

--- chap3/3.3/3_3_1_spinlock_1.c
+++ chap3/3.3/3_3_1_spinlock_1.c
@@ -1,7 +1,7 @@
 #include "../3.2/3_2_2_tas.c"
 
 void spinlock_acquire(bool *lock) {
-    while (!test_and_set(lock)); // <1>
+    while (test_and_set(lock)); // <1>
 }
 
 void spinlock_release(bool *lock) {

6a06110fd62291e8882d0cd2426289b6e7a59ec7
Kotaro Abe <abekoh.bcky@gmail.com>
2022-06-26 13:37:41 +0900

    Fix dependencies and add mut

--- chap4/4.6/ch4_6_signal_rust/src/main.rs
+++ chap4/4.6/ch4_6_signal_rust/src/main.rs
@@ -1,11 +1,12 @@
-use signal_hook::{iterator::Signals, SIGUSR1}; // <1>
+use signal_hook::{iterator::Signals}; // <1>
 use std::{error::Error, process, thread, time::Duration};
+use libc::SIGUSR1;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // プロセスIDを表示
     println!("pid: {}", process::id());
 
-    let signals = Signals::new(&[SIGUSR1])?; // <2>
+    let mut signals = Signals::new(&[SIGUSR1])?; // <2>
     thread::spawn(move || {
         // シグナルを受信
         for sig in signals.forever() { // <3>
